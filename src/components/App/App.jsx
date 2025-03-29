826f19d76e6db952bb754a08a00fb4f83f82de46
menth0s <marslite@pm.me>
2025-03-29 13:10:37 +0100

    Working on file structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello NewsExplorer</h1>
+      <p>Offtopic.gg</p>
+    </>
+  );
+}
+
+export default App;
