51249af6dec9285b6edc0adc76dc987466374d3a
menth0s <marslite@pm.me>
2025-04-01 00:25:03 +0200

    Styled search__form

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,9 +2,15 @@ import "./SearchForm.css";
 
 function SearchForm() {
   return (
-    <div className="SearchForm">
+    <div className="search_form">
       <div className="search_form_wrapper">
-        <p className="search__form">SearchForm</p>
+        <input
+          className="search__form"
+          type="text"
+          name="search"
+          placeholder="Enter topic"
+        />
+        <button className="search__btn">Search</button>
       </div>
     </div>
   );
