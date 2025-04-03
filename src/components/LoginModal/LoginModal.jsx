d14e2e1e7ff3012abe7ca073e9df6c4f593b62e5
menth0s <marslite@pm.me>
2025-04-03 15:01:39 +0200

    Styling modal according to requirements

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -6,19 +6,31 @@ function LoginModal({ title, buttonText }) {
     <ModalWithForm title={title}>
       <label htmlFor="login__email" className="modal__label modal__label_login">
         Email{""}
-        <input type="email" className="modal__input" id="login__email" />
+        <input
+          type="email"
+          className="modal__input"
+          id="login__email"
+          placeholder="Enter Email"
+        />
       </label>
       <label
         htmlFor="login__password"
         className="modal__label modal__label_login"
       >
         Password{""}
-        <input type="password" className="modal__input" id="login__password" />
+        <input
+          type="password"
+          className="modal__input"
+          id="login__password"
+          placeholder="Enter Password"
+        />
       </label>
       <div className="btn__container">
         <button className="modal__login_btn">{buttonText}</button>
 
-        <button className="modal__signup_btn ">or Sign Up</button>
+        <button className="modal__signup_btn ">
+          or <a className="modal__signup_txt">Sign Up</a>
+        </button>
       </div>
     </ModalWithForm>
   );
