7cc0f5ad5fee96b1592e8395202d431ac8bed467
menth0s <marslite@pm.me>
2025-04-07 16:41:45 +0200

    Styrling and implementing NewsCard and NewsCardMap

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,12 +1,15 @@
 import "./NewsCard.css";
 import logo from "../../assets/author_bg.jpg";
+import bookmark from "../../assets/bookmark.svg";
 
 function NewsCard() {
   return (
     <li className="news__card_wrapper">
       <div className="card__header">
         <img src={logo} alt="" className="card__image" />
-        <button className="save__btn"></button>
+        <button className="save__btn">
+          <img src={bookmark} alt="" className="btn__image" />
+        </button>
       </div>
       <div className="card__info">
         <p className="card__date">Card Date</p>
