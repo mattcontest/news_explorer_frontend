1cd3455163f46adf988b985827ac6b9bbc0d7e92
menth0s <marslite@pm.me>
2025-04-09 18:41:35 +0200

    Implementing saved-news components and styling

--- src/components/SavedNewsHeader/SavedNewsHeader.jsx
+++ src/components/SavedNewsHeader/SavedNewsHeader.jsx
@@ -9,6 +9,17 @@ function SavedNewsHeader({ handleLoginClick, isLoggedIn }) {
           handleLoginClick={handleLoginClick}
           isLoggedIn={isLoggedIn}
         />
+        <div className="saved__news_header_content">
+          <div className="internal__saved_header_wrapper">
+            <h5 className="saved__news__header_title">Saved Articles</h5>
+            <h1 className="saved__news_header_description">
+              Elise, you have 5 saved articles
+            </h1>
+            <p className="saved__news_header_keywords">
+              By keywords: Nature, Yellowstone, and 2 other
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
