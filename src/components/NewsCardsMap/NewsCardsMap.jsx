b22816ea3b3517cfabf34d173b5ed74a49648c4e
menth0s <marslite@pm.me>
2025-04-16 17:17:09 +0200

    Styling about for Tablet Sizing

--- src/components/NewsCardsMap/NewsCardsMap.jsx
+++ src/components/NewsCardsMap/NewsCardsMap.jsx
@@ -9,34 +9,39 @@ function NewsCardsMap({ articles }) {
     setCardsShown(cardsShown + 3);
   };
   return (
-    <div className="newscards__map">
-      <h2 className="news__cards_title">Search Results</h2>
-      <ul className="news__cards_list">
-        {articles.slice(0, cardsShown).map((article) => {
-          return (
-            <NewsCard
-              key={article._id}
-              imageUrl={article.urlToImage}
-              title={article.title}
-              description={article.description}
-              author={article.source?.name || "Uknown Source"}
-              date={new Date(article.publishedAt).toLocaleDateString("en-US", {
-                month: "long",
-                day: "numeric",
-                year: "numeric",
-              })}
-            />
-          );
-        })}
-      </ul>
-      {cardsShown <= 18 ? (
-        <button className="show__more_btn" onClick={showMore}>
-          Show More
-        </button>
-      ) : (
-        ""
-      )}
-    </div>
+    <>
+      <div className="newscards__map">
+        <h2 className="news__cards_title">Search Results</h2>
+        <ul className="news__cards_list">
+          {articles.slice(0, cardsShown).map((article) => {
+            return (
+              <NewsCard
+                key={article._id}
+                imageUrl={article.urlToImage}
+                title={article.title}
+                description={article.description}
+                author={article.source?.name || "Uknown Source"}
+                date={new Date(article.publishedAt).toLocaleDateString(
+                  "en-US",
+                  {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric",
+                  }
+                )}
+              />
+            );
+          })}
+        </ul>
+        {cardsShown <= 18 ? (
+          <button className="show__more_btn" onClick={showMore}>
+            Show More
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
   );
 }
 
