d4809c44035280509e1c753a1dcc522dda7a448e
menth0s <marslite@pm.me>
2025-04-23 20:21:19 +0200

    Styling for mobile  resolution & fixing js logic

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -64,7 +64,7 @@ function Navigation({ handleLoginClick, isLoggedIn }) {
 
           {!(pathname === "/saved-news") && (
             <button
-              className={`navigation__signin-btn ${"nav__mobile-sign_in"} ${MobileMenuOpen ? "mobile__open" : "mobile__close"}`}
+              className={`navigation__signin-btn nav__mobile-sign_in ${MobileMenuOpen ? "mobile__open" : "mobile__close"}`}
             >
               Sign In
             </button>
