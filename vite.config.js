e2ee96ce639047eba890cd6652e77cb5ed14203e
menth0s <marslite@pm.me>
2025-05-06 02:15:09 +0200

    Debugging deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/news-explorer-frontend/",
+  base: "/news_explorer_frontend/",
   plugins: [react()],
   server: {
     port: 3002,
