d3506b79444c2b83a363378d0dd15140999eeb32
menth0s <marslite@pm.me>
2025-05-06 02:13:01 +0200

    Debugging deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/news-explorer-frontend/">
+    <BrowserRouter basename="/news_explorer_frontend/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
