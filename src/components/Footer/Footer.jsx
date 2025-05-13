3174ba2db4dbc2ba346d1c1bb64db34857f93bf7
menth0s <marslite@pm.me>
2025-05-13 17:46:57 +0200

    Addressed feedback for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,13 +11,20 @@ function Footer() {
         {/* <a href="" className="portfolio footer__link">
           Portfolio
           </a> */}
-        <a href="" className="tripleten footer__link">
+        <a
+          href="https://tripleten.com/"
+          className="tripleten footer__link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           TripleTen
         </a>
         <div className="footer__socials">
           <a
             href="https://github.com/mattcontest"
             className="github footer__link"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             {/* GitHub */}
             <img src={github} alt="" className="github__logo" />
