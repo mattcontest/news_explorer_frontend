26349c8b2ef542fb3560fcfe946d0b8d2e341a01
menth0s <marslite@pm.me>
2025-05-15 19:03:53 +0200

    Addressed footer feedback

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,33 +4,73 @@ import github from "../../assets/github.svg";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__links">
-        <a href="" className="home footer__link">
-          Home
-        </a>
-        {/* <a href="" className="portfolio footer__link">
-          Portfolio
-          </a> */}
-        <a
-          href="https://tripleten.com/"
-          className="tripleten footer__link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TripleTen
-        </a>
-        <div className="footer__socials">
+      <div className="footer__mobile mobile__open">
+        <div className="footer__links">
+          <div className="footer__links-first">
+            <a href="" className="home footer__link">
+              Home
+            </a>
+
+            <a
+              href="https://github.com/mattcontest"
+              className="github footer__link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="" className="github__logo" />
+            </a>
+          </div>
+
+          <div className="footer__links-second">
+            <a
+              href="https://tripleten.com/"
+              className="tripleten footer__link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              TripleTen
+            </a>
+          </div>
+
+          {/* <div className="footer__socials">
           <a
             href="https://github.com/mattcontest"
             className="github footer__link"
             target="_blank"
             rel="noopener noreferrer"
           >
-            {/* GitHub */}
+
             <img src={github} alt="" className="github__logo" />
           </a>
+
+
+
+        </div> */}
         </div>
       </div>
+
+      <div className="footer_non-mobile desktop-open">
+        <div className="footer_non-mobile-links">
+          <div className="footer__non-mobile-first">
+            <a href="" className=" footer__link">
+              Home
+            </a>
+            <a href="" className=" footer__link">
+              TripleTen
+            </a>
+
+            <a
+              href="https://github.com/mattcontest"
+              className=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={github} alt="" className="github__logo" />
+            </a>
+          </div>
+        </div>
+      </div>
+
       <p className="footer_text">© 2025 Supersite, Powered by News API</p>
     </footer>
   );
