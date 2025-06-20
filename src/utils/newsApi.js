935cf6d31c73ff195815541bf3f5b8cef736047c
menth0s <marslite@pm.me>
2025-06-21 00:40:33 +0200

    Edited .env

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -5,12 +5,10 @@ import { checkResponse } from "./api";
 export const getNews = ({ q }) => {
   const currentDate = new Date();
   const fromDate = new Date();
+  const APIkey = import.meta.env.VITE_API_KEY;
   fromDate.setDate(currentDate.getDate() - 7);
   return fetch(
-    `https://newsapi.org/v2/everything?q=${q}&apiKey=${APIkey}&from=${fromDate.toISOString()}&pageSize=50`
+    // `https://newsapi.org/v2/everything?q=${q}&apiKey=${APIkey}&from=${fromDate.toISOString()}&pageSize=50`
+    `https://nomoreparties.co/news/v2/everything?q=${q}&apiKey=${APIkey}&from=${fromDate.toISOString()}&pageSize=50`
   ).then(checkResponse);
 };
-
-//Should be hidden or added to the gitignore
-// export const APIkey = "8788558558194f3fbcc8a11c3f01f59f";
-export const APIkey = "e96e734cb0194eafae7a5c80c5eab1ca";
