3160de4b90b7725eb59a5d91a746cae71368098e
menth0s <marslite@pm.me>
2025-06-20 22:24:24 +0200

    Updating BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:3001";
-//   process.env.NODE_ENV === "production"
-//     ? "https://api.trywtwr.jumpingcrab.com"
-//     : "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "https://api.atlaba.com";
+
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.atlaba.com"
+    : "http://localhost:3001";
