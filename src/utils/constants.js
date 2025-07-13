a95b13446ecec0c031e5be349bd0f6f001d6448b
menth0s <marslite@pm.me>
2025-07-13 22:02:46 +0200

    Re-deployed with right edits

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // export const BASE_URL = "http://localhost:3001";
 // export const BASE_URL = "https://api.atlaba.com";
 
-// export const BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://api.atlaba.com"
-//     : "http://localhost:3000";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://api.atlaba.com"
+    : "http://localhost:3000";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
